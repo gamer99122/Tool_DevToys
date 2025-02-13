109a361259df02d8a4e6f4005f77d22db9064c4c
gamer99122 <gamer99122@hotmail.com.tw>
2025-02-13 16:21:23 +0800

    新增，SQL轉換字串

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,20 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.rTxtInput = new System.Windows.Forms.RichTextBox();
+            this.rTxtOutput = new System.Windows.Forms.RichTextBox();
+            this.btnAction = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(14, 16);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(981, 678);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnAction);
+            this.tabPage1.Controls.Add(this.rTxtOutput);
+            this.tabPage1.Controls.Add(this.rTxtInput);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(973, 649);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "SQL轉換字串";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage2.Size = new System.Drawing.Size(1141, 895);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage3.Size = new System.Drawing.Size(1141, 895);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // rTxtInput
+            // 
+            this.rTxtInput.BackColor = System.Drawing.Color.OldLace;
+            this.rTxtInput.Location = new System.Drawing.Point(7, 8);
+            this.rTxtInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rTxtInput.Name = "rTxtInput";
+            this.rTxtInput.Size = new System.Drawing.Size(958, 280);
+            this.rTxtInput.TabIndex = 0;
+            this.rTxtInput.Text = "";
+            // 
+            // rTxtOutput
+            // 
+            this.rTxtOutput.BackColor = System.Drawing.Color.OldLace;
+            this.rTxtOutput.Location = new System.Drawing.Point(0, 354);
+            this.rTxtOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rTxtOutput.Name = "rTxtOutput";
+            this.rTxtOutput.Size = new System.Drawing.Size(958, 280);
+            this.rTxtOutput.TabIndex = 1;
+            this.rTxtOutput.Text = "";
+            // 
+            // btnAction
+            // 
+            this.btnAction.Location = new System.Drawing.Point(8, 296);
+            this.btnAction.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(957, 50);
+            this.btnAction.TabIndex = 2;
+            this.btnAction.Text = "轉換";
+            this.btnAction.UseVisualStyleBackColor = true;
+            this.btnAction.Click += new System.EventHandler(this.btnAction_Click);
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Main";
             this.Text = "Main";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.RichTextBox rTxtInput;
+        private System.Windows.Forms.RichTextBox rTxtOutput;
+        private System.Windows.Forms.Button btnAction;
     }
 }
 
--- Main.cs
+++ Main.cs
@@ -16,5 +16,33 @@ namespace Tool_DevToys
         {
             InitializeComponent();
         }
+
+        private void btnAction_Click(object sender, EventArgs e)
+        {
+            btnAction.Enabled = false;
+            string strInput = rTxtInput.Text;
+            string strOutput = Action(strInput);
+            rTxtOutput.Text = strOutput;
+            Clipboard.SetText(strOutput);
+            btnAction.Enabled = true;
+        }
+
+        public string Action(string SQL)
+        {
+            StringBuilder sb = new StringBuilder();
+            string[] lines = SQL.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string trimmedLine = lines[i].TrimEnd(); // 去掉行尾多餘的空格
+                string formattedLine = $"\\n {trimmedLine}  "; // 統一在行尾加上兩個空格
+
+                if (i == 0)
+                    sb.AppendLine($"SQL = $\"{formattedLine}\";");
+                else
+                    sb.AppendLine($"SQL += $\"{formattedLine}\";");
+            }
+            return sb.ToString();
+        }
     }
 }
